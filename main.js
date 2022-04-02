2510d31c7e713738c9741ef78b1a03e1e907ba34
Szymon Burak <sajmon0031@gmail.com>
2022-04-02 12:35:16 +0200

    Add privacy policy and Google Maps

--- main.js
+++ main.js
@@ -36,4 +36,18 @@ const splide = new Splide( '.splide', {
 
 splide.mount();
 
-console.log(splide);
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 53.110450, lng: 19.086053 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
